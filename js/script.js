0d335c0c383adaed981139c4a02b054bfc3d4abe
ihangji <zjdyx.ting@qq.com>
2019-05-30 21:10:00 +0800

    Site updated: 2019-05-30 21:09:59

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 (function ($) {
-  console.log('© Theme-Vexo | https://github.com/yanm1ng/hexo-theme-vexo')
+  console.log('© hexo | https://github.com/ihangji')
   var app = $('.app-body')
   var header = $('.header')
   var banner = document.getElementById('article-banner') || false
